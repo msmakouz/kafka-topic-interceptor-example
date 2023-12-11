bee187153940b6665a7ca9a3f1ab0b7a7bcaeca4
Maxim Smakouz <m.smakouz@gmail.com>
2023-12-11 14:04:06 +0200

    Add Spiral app

---
+++ app/app/src/Application/Kernel.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application;
+
+use Spiral\Boot\Bootloader\CoreBootloader;
+use Spiral\Bootloader as Framework;
+use Spiral\Debug\Bootloader\DumperBootloader;
+use Spiral\DotEnv\Bootloader\DotenvBootloader;
+use Spiral\Monolog\Bootloader\MonologBootloader;
+use Spiral\Nyholm\Bootloader\NyholmBootloader;
+use Spiral\Prototype\Bootloader\PrototypeBootloader;
+use Spiral\Queue\Bootloader\QueueBootloader;
+use Spiral\RoadRunnerBridge\Bootloader as RoadRunnerBridge;
+use Spiral\Scaffolder\Bootloader\ScaffolderBootloader;
+use Spiral\Tokenizer\Bootloader\TokenizerListenerBootloader;
+
+class Kernel extends \Spiral\Framework\Kernel
+{
+    public function defineSystemBootloaders(): array
+    {
+        return [
+            CoreBootloader::class,
+            DotenvBootloader::class,
+            TokenizerListenerBootloader::class,
+            DumperBootloader::class,
+        ];
+    }
+
+    public function defineBootloaders(): array
+    {
+        return [
+            // Logging and exceptions handling
+            MonologBootloader::class,
+            Bootloader\ExceptionHandlerBootloader::class,
+
+            // Application specific logs
+            Bootloader\LoggingBootloader::class,
+
+            // RoadRunner
+            RoadRunnerBridge\LoggerBootloader::class,
+            RoadRunnerBridge\QueueBootloader::class,
+
+            // Core Services
+            Framework\SnapshotsBootloader::class,
+
+            // Security and validation
+            Framework\Security\EncrypterBootloader::class,
+
+            // Queue
+            QueueBootloader::class,
+
+            NyholmBootloader::class,
+
+            // Console commands
+            Framework\CommandBootloader::class,
+            RoadRunnerBridge\CommandBootloader::class,
+            ScaffolderBootloader::class,
+            RoadRunnerBridge\ScaffolderBootloader::class,
+
+            // Fast code prototyping
+            PrototypeBootloader::class,
+        ];
+    }
+}
